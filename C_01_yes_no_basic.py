7ed223ea8bb0c6ef30f8e082e3c6a34dd0b74ab0
PerryzePlatypus <gonzalesp4476@masseyhigh.school.nz>
2026-02-04 15:01:28 +1300

    incomplete yes or no checker

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,8 +1,16 @@
-want_instructions =input("Do you want to see the instructions?").lower()
+while True:
 
-if want_instructions == "yes" or want_instructions == "y":
-    print("you said yes")
-elif want_instructions == "no" or want_instructions == "n":
-    print("you said no")
-else:
-    print("please enter yes/no")
+    want_instructions = input("Do you want to see the instructions?").lower()
+
+    #checks if user says yes/no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("you said no")
+        break
+    else:
+        print("please enter yes/no")
+        continue
+
+print("we done")
